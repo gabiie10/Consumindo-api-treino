6ee41a2de034ca3a8d033b9edea6742fb35159a2
Gabriela dos Santos Lamar <97289224+gabiie10@users.noreply.github.com>
2022-02-08 16:43:29 -0300

    mudanças

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+import PromotionCard from './components/Promotion/Card/Card';
 function App() {
+  const [info,setInfo]=useState([]);
+
+  useEffect(()=>{axios.get('http://localhost:5000/promotions')
+  .then((response)=>{
+    setInfo(response.data);
+    console.log(response.data);
+  });
+},[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {info.map((info)=>(<div>
+          <h1>{info.title}</h1>
+          <h2>{info.price}</h2>
+          <img src={info.img}></img>
+          <a href="https://www.petlove.com.br/">IR PARA SITE</a>
+          </div>
+      ))}
+      
     </div>
   );
 }
---
+++ src/components/Promotion/Card/Card.js
@@ -0,0 +1,7 @@
+import React from "react";
+const PromotionCard=({teste})=>{
+  return(  <div>
+     hello {teste}
+  </div>);
+}
+export default PromotionCard;
